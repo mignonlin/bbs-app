ee9269ffe8e9325b1ba8ff818801b440bd3ca3c1
Administrator <Administrator@HI-CHEONHOPC.local>
2020-07-21 12:29:06 +0900

    Update .babelrc, .editorconfig, and 37 more files...

---
+++ src/components/BoardList.vue
@@ -0,0 +1,63 @@
+<template>
+  <div role="tablist">
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-1 variant="danger">①</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>
+            <img src='../assets/pic1.jpg' alt='케어베어'/>
+            {{ text1 }}
+          </b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-2 variant="danger">②</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-2" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text2 }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+
+    <b-card no-body class="mb-1">
+      <b-card-header header-tag="header" class="p-1" role="tab">
+        <b-button block v-b-toggle.accordion-3 variant="danger">③</b-button>
+      </b-card-header>
+      <b-collapse id="accordion-3" accordion="my-accordion" role="tabpanel">
+        <b-card-body>
+          <b-card-text>{{ text3 }}</b-card-text>
+        </b-card-body>
+      </b-collapse>
+    </b-card>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        text1: `
+          하이루 하이루
+        `,
+         text2: `
+          배곻파 배고파
+        `,
+         text3: `
+          집에 가고싶어용 ~~~ ^_^
+        `
+      }
+    }
+  }
+</script>
+<style>
+  #accordion-1 img {
+    display:block;
+    justify-content:center
+  }
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div class="footer-info">
+      copyright 2020 vue-board.com all right reserved.
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Footer',
+  data(){
+    return{
+      
+    }
+  }
+}
+</script>
+<style scoped>
+  .footer-info {
+    margin-top:10px;
+    width:100%;
+    height:50px;
+    line-height:50px;
+    display:flex;
+    justify-content:center;
+    background:#FF8080;
+    color:white
+  }
+</style>
